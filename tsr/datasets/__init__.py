13bcb98cde1586ba6e380eccae1ce1e4665f9573
hy3134 <hy3134@g.rit.edu>
2021-10-28 15:34:26 -0400

    added dataset manager class

--- tsr/datasets/__init__.py
+++ tsr/datasets/__init__.py
@@ -1,1 +1,4 @@
-from tsr.datasets import ngafid, common
+from tsr.datasets import ngafid, common, tep, datasetmanager
+from tsr.datasets.ngafid import NGAFID_DatasetManager
+from tsr.datasets.tep import TEP_DatasetManager
+from tsr.datasets.datasetmanager import DatasetManager
---
+++ tsr/datasets/tep.py
@@ -0,0 +1,4 @@
+from tsr.datasets import DatasetManager
+
+class TEP_DatasetManager(DatasetManager):
+	url = "https://drive.google.com/uc?id=1m6Gkp2tNnnlAzaAVLaWnC2TtXNX2wJV8"
