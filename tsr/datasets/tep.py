42e4b03742b49c40c89179b90b700897261897ea
hy3134 <hy3134@g.rit.edu>
2021-10-29 15:42:18 -0400

    tep dataset

--- tsr/datasets/tep.py
+++ tsr/datasets/tep.py
@@ -1,4 +1,15 @@
+import tensorflow as tf
+
+from tsr.config import Config
 from tsr.datasets import DatasetManager
 
 class TEP_DatasetManager(DatasetManager):
+	def __init__(self, config: Config):
+		pass
+
+	def get_train_and_val_for_fold(self, fold: int) -> (tf.data.Dataset, tf.data.Dataset):
+		pass
+	
+
 	url = "https://drive.google.com/uc?id=1m6Gkp2tNnnlAzaAVLaWnC2TtXNX2wJV8"
+
