c8a938cdc4d17dcc815ce63648fa1b18197b946c
hy3134 <hy3134@g.rit.edu>
2021-11-17 17:25:53 -0500

    switched to list comprehension for convolutions updated flake8

--- tsr/__init__.py
+++ tsr/__init__.py
@@ -1,3 +1,2 @@
 from tsr import datasets, methods, models, config
 from tsr.tpusetup import connect_to_tpu
-from tsr.config import Config
--- tsr/models/attention/convmhsa.py
+++ tsr/models/attention/convmhsa.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 
 from tsr.models.attention import EncoderLayer
-from tsr import Config
+from tsr.config import Config
+
 
 class ConvMHSA(tf.keras.Sequential):
-
     @classmethod
-    def from_config(cls, config:Config):
+    def from_config(cls, config: Config):
 
         if config.hyperparameters.num_classes > 2:
-            output = tf.keras.layers.Dense(config.hyperparameters.num_classes, activation = "softmax")
+            output = tf.keras.layers.Dense(config.hyperparameters.num_classes, activation="softmax")
         else:
-            output = tf.keras.layers.Dense(1, activation = "sigmoid"),
+            output = (tf.keras.layers.Dense(1, activation="sigmoid"),)
 
         mc = config.model
         return cls(
@@ -22,10 +22,11 @@ class ConvMHSA(tf.keras.Sequential):
             num_heads=mc.num_heads,
             d_model=mc.d_model,
             dff=mc.dff,
-            output= output,
+            output=output,
         )
 
-    def __init__(self,
+    def __init__(
+        self,
         input_shape=(4096, 23),
         conv_filters=[128, 128, 256, 256, 512],
         kernel=[7, 7, 3, 7, 7],
@@ -34,55 +35,28 @@ class ConvMHSA(tf.keras.Sequential):
         d_model=512,
         dff=512,
         output: tf.keras.layers.Layer = tf.keras.layers.Dense(1, activation="sigmoid"),
-                 ):
-
+    ):
 
         super().__init__(
             [
-                tf.keras.Input(shape = input_shape),
+                tf.keras.Input(shape=input_shape),
+            ]
+            + [
                 tf.keras.layers.Conv1D(
-                    filters = conv_filters[0],
-                    kernel_size = kernel[0],
-                    strides = strides[0],
-                    padding = "same",
-                    activation = "relu",
-                ),
-                tf.keras.layers.Conv1D(
-                    filters = conv_filters[1],
-                    kernel_size = kernel[1],
-                    strides = strides[1],
-                    padding = "same",
-                    activation = "relu",
-                ),
-                tf.keras.layers.Conv1D(
-                    filters = conv_filters[2],
-                    kernel_size = kernel[2],
-                    strides = strides[2],
-                    padding = "same",
-                    activation = "relu",
-                ),
-                tf.keras.layers.Conv1D(
-                    filters = conv_filters[3],
-                    kernel_size = kernel[3],
-                    strides = strides[3],
-                    padding = "same",
-                    activation = "relu",
-                ),
-                tf.keras.layers.Conv1D(
-                    filters = conv_filters[4],
-                    kernel_size = kernel[4],
-                    strides = strides[4],
-                    padding = "same",
-                    activation = "relu",
-                ),
-                EncoderLayer(d_model = d_model, num_heads = num_heads, dff = dff),
-                EncoderLayer(d_model = d_model, num_heads = num_heads, dff = dff),
-                EncoderLayer(d_model = d_model, num_heads = num_heads, dff = dff),
-                EncoderLayer(d_model = d_model, num_heads = num_heads, dff = dff),
+                    filters=conv_filters[i],
+                    kernel_size=kernel[i],
+                    strides=strides[i],
+                    padding="same",
+                    activation="relu",
+                )
+                for i in range(len(kernel))
+            ]
+            + [
+                EncoderLayer(d_model=d_model, num_heads=num_heads, dff=dff),
+                EncoderLayer(d_model=d_model, num_heads=num_heads, dff=dff),
+                EncoderLayer(d_model=d_model, num_heads=num_heads, dff=dff),
+                EncoderLayer(d_model=d_model, num_heads=num_heads, dff=dff),
                 tf.keras.layers.GlobalAveragePooling1D(),
                 output,
             ]
         )
-
-
-
