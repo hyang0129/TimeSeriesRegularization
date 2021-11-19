d4dcefae7d958a0ee5f3cce1b6f200dbcd4c3d46
hy3134 <hy3134@g.rit.edu>
2021-11-19 14:33:30 -0500

    added confusion matrix method

--- format.py
+++ format.py
@@ -1,4 +1,32 @@
-from tsr.utils import shell_exec
+import subprocess
+
+def shell_exec(command_as_string):
+    """
+    Executes the command in shell script. Avoid using the yes | command pattern as that seems to cause an out of memory
+    issue.
+
+    Args:
+        command_as_string: just a string, as if you were typing it into a  terminal
+
+    Returns:
+        stdout, stderr
+    """
+    try:
+
+        process = subprocess.Popen(
+            command_as_string.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False
+        )
+        stdout, stderr = process.communicate()
+
+    except subprocess.CalledProcessError as e:
+        stdout = e.stdout
+        stderr = e.stderr
+
+    stdout = stdout.decode("utf-8")
+    stderr = stderr.decode("utf-8")
+
+    return stdout, stderr
+
 
 stdout, stderr = shell_exec("python3 -m black .")
 print(stdout)
