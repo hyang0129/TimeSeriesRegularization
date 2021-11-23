142d2096f6709a6458f711164b9318d810c546c3
hy3134 <hy3134@g.rit.edu>
2021-11-23 16:26:27 -0500

    added abstract rocket

---
+++ tsr/models/nondeep/rocket.py
@@ -0,0 +1,17 @@
+class ROCKET():
+
+    @classmethod
+    def from_config(cls):
+        raise NotImplementedError
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def fit(self):
+        raise NotImplementedError
+
+    def __call__(self):
+        raise NotImplementedError
+
+    def predict(self):
+        raise NotImplementedError
