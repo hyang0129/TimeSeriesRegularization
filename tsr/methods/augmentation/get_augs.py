6e2c09d22f3a504e0f478687c8a5ea446085951c
hy3134 <hy3134@g.rit.edu>
2022-01-04 13:48:38 -0500

    added version 2 augs

--- tsr/methods/augmentation/get_augs.py
+++ tsr/methods/augmentation/get_augs.py
@@ -10,6 +10,10 @@ def get_augs(SHAPE, BATCH_SIZE = 64, DO_PROB = 0.5, element_prob = 0.5, version 
         DO_PROB = 0.8
         element_prob = 0.5
 
+    elif version == 2:
+        DO_PROB = 0.7
+        element_prob = 0.4
+
     mixup = Mixup(batch_size = BATCH_SIZE,
                   do_prob = DO_PROB,
                   sequence_shape = SHAPE[1:],
@@ -55,6 +59,18 @@ def get_augs(SHAPE, BATCH_SIZE = 64, DO_PROB = 0.5, element_prob = 0.5, version 
         def batch_aug(x, y):
             example = {'input': x, 'target': y}
             example = cutmix(example)
+            example = cutout(example)
+            example = mixup(example)
+            x, y = example['input'], example['target']
+            return x, y
+
+    elif version == 2:
+
+        def batch_aug(x, y):
+            example = {'input': x, 'target': y}
+            example = cutmix(example)
+            example = cutmix(example)
+            example = cutout(example)
             example = cutout(example)
             example = mixup(example)
             x, y = example['input'], example['target']
